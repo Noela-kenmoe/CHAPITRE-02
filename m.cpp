b3c6f60397d2e5b386d7807042920edb122357a1
Noela-kenmoe <kenmoenoela@gmail.com>
2026-09-16 17:54:27 +0200

    Ajouter du premier fichier qui est m.cpp
    le fichier m.cpp a été créer, il est maintenant question de l'ajouter au dépot afin qu'il soit visible sur github
    Le dépot contiendra maintenant le fichier m.cpp

---
+++ m.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main (){
+    int tab[5];
+    int i;
+    float som , moy ;
+     for (i=1 ; i<5 ; i++){
+        std::cout<<"entrer la note numero" << i << ":";
+        std::cin>> tab[i];
+     }
+     for(i=1 ; i<5 ; i++){
+        som=0;
+        som=som+tab[i];
+        tab[i]++;
+     }
+     
+     std::cout<<"la SOMME de la classe est :"<< som;
+   return 0 ;
+}
