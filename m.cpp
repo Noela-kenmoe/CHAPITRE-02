cbaa576ed9409d59fc8f4232cbddaee97c440869
Noela-kenmoe <kenmoenoela@gmail.com>
2026-09-17 18:41:30 +0200

    modifier la ligne du meme dépot
    première modification de la ligne d'un meme fichier dans deux dépot différents
    Cette meme ligne sera modifiée dans le second dépot

--- m.cpp
+++ m.cpp
@@ -3,17 +3,20 @@
 int main (){
     int tab[5];
     int i;
-    float som , mn ;
-     for (i=1 ; i<5 ; i++){
-        std::cout<<"entrer la note numero" << i << ":";
+    float som=0 , mn=0 ;//première modification
+     for (i=0 ; i<5 ; i++){
+        std::cout<<"entrer la note numero" << i+1 << ":";
         std::cin>> tab[i];
      }
-     for(i=1 ; i<5 ; i++){
-        som=0;
+     for(i=0 ; i<5 ; i++){
         som=som+tab[i];
-        tab[i]++;
+        
      }
+     //deuxième modification
+     mn = som / 5.0;
+   
      
      std::cout<<"la SOMME de la classe est :"<< som;
+     std::cout<<"la moyenne  est :"<< mn;//troisème  modification
    return 0 ;
 }
